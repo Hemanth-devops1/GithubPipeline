26a782754236a297589c12078cc67a5621caef59
Hemanth-devops1 <hemanthakumaraks1@gmail.com>
2025-07-26 13:38:02 +0530

    app

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Define route
 app.get('/', (req, res) => {
   res.send('Hello World! This is my CI 🚀');
 });
 
-module.exports = app;
+// Start the server
+app.listen(3000, () => {
+  console.log('🚀 Server is running on port 3000');
+});
